3eb22eb9065cf324ecd4e771bd2380a699dafe4c
Wishwajeet Singh <Wishwajeetrana1990@gmail.com>
2024-09-25 17:23:03 +0530

    added new branch and new function

--- Hello.c
+++ Hello.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
-
+int featurec();
 int main()
 {
 	printf("%s :Begins\n",__func__);
-	printf("Feature added by wishu-featureb branch");
+	printf("Feature added by wishu-featureb branch\n");
+	featurec();
 	printf("%s :End\n",__func__);
+	return 0;
+}
+int featurec()
+{
+	printf("This is %s function \n",__func__);
 	return 0;
 }
